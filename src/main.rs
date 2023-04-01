f113d37ded2ba18251b7aae1aa6c3010e549737a
Luke Jones <luke@lukeojones.com>
2023-04-01 18:42:22 +0100

    feat(bb2) using buffer for (de)serialization instead

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ use std::fs;
 
 fn main() {
     exercise_one();
+    exercise_two();
 }
 
 fn exercise_one() {
+    println!();
+    println!("<<< Exercise One >>>");
     let path = "output.json";
     let a = Move { direction: Direction::Up, distance: 0 };
     println!("Serializing {:?}...", a);
@@ -17,6 +20,21 @@ fn exercise_one() {
     println!("Deserializing from {}...", path);
     let b_json = fs::read_to_string(path).expect("Unable to read from file");
     let b: Move = serde_json::from_str(&b_json).unwrap();
+
+    println!("Result of deserialization is {:?}...", b);
+}
+
+fn exercise_two() {
+    println!();
+    println!("<<< Exercise Two >>>");
+    let path = "output_two.json";
+    let a = Move { direction: Direction::Up, distance: 0 };
+
+    println!("Serializing to buffer {:?}...", a);
+    let buffer = serde_json::to_vec(&a).unwrap();
+
+    println!("Deserializing from buffer");
+    let b: Move = serde_json::from_slice(&buffer).unwrap();
 
     println!("Result of deserialization is {:?}...", b);
 }
