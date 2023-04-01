8149bd9673f2f967f48b666058716c9cdb5f104e
Luke Jones <luke@lukeojones.com>
2023-04-01 18:23:14 +0100

    feat(bb2) implement JSON serialization

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
     let a = Move { direction: Direction::Up, distance: 0 };
+    let a_json = serde_json::to_string(&a).unwrap();
+    fs::write("output.json", a_json).expect("Unable to write to file");
     println!("The direction is {:?}", a);
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Move {
     direction: Direction,
     distance: u32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 enum Direction {
     Up,
     Down,
