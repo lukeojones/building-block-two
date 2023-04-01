77c5bc719681f78a8d3f7bc52cb4c1a6f947245d
Luke Jones <luke@lukeojones.com>
2023-04-01 19:11:41 +0100

    feat(bb2) using RON serialization

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::fs;
+use ron;
 
 fn main() {
-    exercise_one();
-    exercise_two();
+    // exercise_one();
+    // exercise_two();
+    exercise_two_ron();
 }
 
 fn exercise_one() {
@@ -27,7 +29,6 @@ fn exercise_one() {
 fn exercise_two() {
     println!();
     println!("<<< Exercise Two >>>");
-    let path = "output_two.json";
     let a = Move { direction: Direction::Up, distance: 0 };
 
     println!("Serializing to buffer {:?}...", a);
@@ -37,6 +38,23 @@ fn exercise_two() {
     let b: Move = serde_json::from_slice(&buffer).unwrap();
 
     println!("Result of deserialization is {:?}...", b);
+}
+
+fn exercise_two_ron() {
+    println!();
+    println!("<<< Exercise Two - RON >>>");
+    let path = "output_two.json";
+    let a = Move { direction: Direction::Up, distance: 0 };
+    println!("Serializing (RON) {:?}...", a);
+
+    let a_ron = ron::ser::to_string(&a).unwrap();
+    fs::write(path, a_ron).expect("Unable to write to file");
+
+    println!("Deserializing from {}...", path);
+    let b_ron = fs::read_to_string(path).expect("Unable to read from file");
+    let b: Move = ron::de::from_str(&b_ron).unwrap();
+
+    println!("Result of deserialization (RON) is {:?}...", b);
 }
 
 #[derive(Debug, Serialize, Deserialize)]
