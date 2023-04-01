02b02968d35341527366bcc5e365808aedb3673d
Luke Jones <luke@lukeojones.com>
2023-04-01 18:31:26 +0100

    feat(bb2) implement deserialization

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@ use serde_json;
 use std::fs;
 
 fn main() {
-    println!("Hello, world!");
+    let path = "output.json";
     let a = Move { direction: Direction::Up, distance: 0 };
+    println!("Serializing {:?}...", a);
+
     let a_json = serde_json::to_string(&a).unwrap();
-    fs::write("output.json", a_json).expect("Unable to write to file");
-    println!("The direction is {:?}", a);
+    fs::write(path, a_json).expect("Unable to write to file");
+
+    println!("Deserializing from {}...", path);
+    let b_json = fs::read_to_string(path).expect("Unable to read from file");
+    let b: Move = serde_json::from_str(&b_json).unwrap();
+
+    println!("Result of deserialization is {:?}...", b);
 }
 
 #[derive(Debug, Serialize, Deserialize)]
